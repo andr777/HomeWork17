63713f9c1907d49b00473dac45d67144ac071754
andr777 <andr.root@gmail.com>
2022-09-27 02:22:36 +0300

    Initial commit

---
+++ src/main/java/pro/sky/HomeWork17/HomeWork17Application.java
@@ -0,0 +1,13 @@
+package pro.sky.HomeWork17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork17Application.class, args);
+	}
+
+}
